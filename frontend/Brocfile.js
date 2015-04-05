24f7113e70e887c71b5ec669a078ba467ffd091d
Chris Hellmuth <chellmuth@gmail.com>
2015-04-05 14:12:29 -0700

    Add bootstrap css

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -19,4 +19,10 @@ var app = new EmberApp();
 
 app.import('bower_components/d3/d3.js');
 
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+  destDir: 'assets'
+});
+
+
 module.exports = app.toTree();
