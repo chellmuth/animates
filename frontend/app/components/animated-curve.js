ca89e4bd0d112c77dad255ff7ac442f6d0472d3f
Chris Hellmuth <chellmuth@gmail.com>
2015-04-05 00:13:58 -0700

    Add some objects to abstract interpolated beziers

--- frontend/app/components/animated-curve.js
+++ frontend/app/components/animated-curve.js
@@ -1,6 +1,29 @@
 import Ember from 'ember';
 /* global d3 */
 
+var Point = Ember.Object.extend({});
+
+var BezierLine = Ember.Object.extend({
+  interpolate: function(t) {
+    var controlPoint1 = this.get('controlPoint1');
+    var controlPoint2 = this.get('controlPoint2');
+
+    var interpolated = Point.create({
+      x: (controlPoint2.get('x') - controlPoint1.get('x')) * t + controlPoint1.get('x'),
+      y: (controlPoint2.get('y') - controlPoint1.get('y')) * t + controlPoint1.get('y')
+    });
+
+    return [ this.get('endPoint1'), interpolated, this.get('endPoint2') ];
+  },
+
+  generator: function() {
+    return d3.svg.line()
+      .interpolate('basis')
+      .x(function(d) { return d.x; })
+      .y(function(d) { return d.y });
+  }.property()
+});
+
 export default Ember.Component.extend({
   tagName: 'svg',
   attributeBindings: 'width height'.w(),
@@ -8,39 +31,30 @@ export default Ember.Component.extend({
   width: 600,
   height: 600,
 
+  line: BezierLine.create({
+    endPoint1: Point.create({x:56, y:215}),
+    endPoint2: Point.create({x:504, y:474}),
+    controlPoint1: Point.create({x:528, y:114}),
+    controlPoint2: Point.create({x:190, y:400})
+  }),
+
   draw: function(timestamp){
     var svg = d3.select('#' + this.get('elementId'));
     svg.style('border', '1px solid black');
 
-    var width = 600;
-    var height = 500;
-
-    var endPoints = [ {x:56, y:215}, {x:504, y:474} ];
-
     var cycle = 1600;
     var t = timestamp % cycle;
-    var frames = 16 * 2;
+    var frames = 4;
     var frame = parseInt(t * (frames/cycle));
 
-    var controlPoints = [{x:528, y:114}, {x:90, y:400}];
-    var controlPoint = {
-      x: (controlPoints[1].x - controlPoints[0].x) * (frame/frames) + controlPoints[0].x,
-      y: (controlPoints[1].y - controlPoints[0].y) * (frame/frames) + controlPoints[0].y
-    };
+    var line = this.get('line').get('generator');
 
-    var line = d3.svg.line()
-      .interpolate('basis')
-      .x(function(d, i) { return d.x; })
-      .y(function(d) { return d.y });
-
-    var points = [endPoints[0], controlPoint, endPoints[1]]
-
-    var lines = svg.select('.lines').selectAll('path').data([points]);
+    var lines = svg.select('.lines').selectAll('path').data([this.get('line').interpolate(frame/frames)]);
     lines.enter().append('path');
     lines.style('stroke', 'black').style('fill', 'none').style('stroke-width', 2)
       .attr('d', line);
 
-    var circles = svg.select('.circles').selectAll('circle').data(points);
+    var circles = svg.select('.circles').selectAll('circle').data(this.get('line').interpolate(frame/frames));
     circles.enter().append('circle');
     circles
       .attr('r', 4)
