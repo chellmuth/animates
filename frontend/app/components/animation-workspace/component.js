af51deef8892dc53b587847a1ac5b4d789dd5efa
Chris Hellmuth <chellmuth@gmail.com>
2015-04-05 21:54:38 -0700

    First pass at interacting with nodes

--- frontend/app/components/animation-workspace/component.js
+++ frontend/app/components/animation-workspace/component.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+/* global d3 */
+
 
 export default Ember.Component.extend({
   tagName: 'svg',
@@ -6,6 +8,9 @@ export default Ember.Component.extend({
 
   xmlns: "http://www.w3.org/2000/svg",
   version: "1.1",
+
+  selected: null,
+
   width: function() {
     return this.get("model.width");
   }.property("model.width"),
@@ -17,6 +22,33 @@ export default Ember.Component.extend({
   draw: function() {
     var svg = d3.select(`#${this.get("elementId")}`);
     this.get("model").draw(svg, 0);
+
+    var that = this;
+    svg.selectAll("circle")
+      .on("mouseover", function() {
+        d3.select(this).attr("r", 6);
+      })
+      .on("mouseout", function() {
+        d3.select(this).attr("r", 4);
+      })
+      .on("mousedown", function() {
+        console.log('down');
+        that.set("selected", d3.select(this));
+      });
+
+    svg
+      .on("mousemove", function() {
+        var selected = that.get("selected");
+        if (selected !== null) {
+          console.log(d3.mouse(this));
+        }
+      })
+      .on("mouseup", function() {
+        that.set("selected", null);
+      })
+      .on("mouseout", function() {
+        that.set("selected", null);
+      });
   },
 
   didInsertElement: function() {
